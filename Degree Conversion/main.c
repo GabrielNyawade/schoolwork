0a5b7922549444768b9261f55883c76073579ef4
GabrielNyawade <gabrielopiyo1133@gmail.com>
2024-10-11 19:57:37 +0300

    Updated 11 Oct

---
+++ Degree Conversion/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float fahrenheit, celsius;
+    printf("Convert Fahrenheit to Celsius\n");
+    printf("Enter degree value in Fahrenheit >>> ");
+    scanf("%f", &fahrenheit);
+    celsius = (fahrenheit - 32) * 5/9;
+    printf("%.2f F = %.2f C\n", fahrenheit, celsius);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # schoolwork
+# Jomo Kenyatta University of Agriculture and Technology
 This is a repository containing Programming Classwork and Assignments
 
-Gabriel Opiyo Nyawade
-SCT211-0029/2024
-BSc. Computer Science
