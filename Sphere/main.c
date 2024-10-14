981184a1113db8e46625ce551ce8f5bbbb058ccc
GabrielNyawade <gabrielopiyo1133@gmail.com>
2024-10-14 13:03:32 +0300

    Updated Files

---
+++ Sphere/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const float pi = 3.142;
+void get_surf_area(float r);
+void get_volume(float r);
+
+
+int main()
+{
+
+    float radius;
+    printf("Enter radius -> ");
+    scanf("%f", &radius);
+
+    get_volume(radius);
+    get_surf_area(radius);
+
+    return 0;
+}
+
+
+void get_volume(float r)
+{
+    float volume = pi * (r*r*r) * 4/3;
+    printf("Volume: %.3f\n", volume);
+
+}
+
+void get_surf_area(float r)
+{
+    float area = pi * (r*r) * 4;
+    printf("Surface area: %.3f\n", area);
+}
---
+++ test/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    float fahrenheit;
+    printf("Enter degrees Fahrenheit >>> ");
+    scanf("%f", &fahrenheit);
+
+    float celsius = (fahrenheit-32) * 5/9;
+    printf("%.2f F = %.2f C", fahrenheit, celsius);
+
+    return 0;
+}
