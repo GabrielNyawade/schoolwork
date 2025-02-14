40f4a56c0afe8b6bb3228b227815fa5bedf1b56c
Gabriel Opiyo <160593612+GabrielNyawade@users.noreply.github.com>
2025-02-14 08:28:06 +0300

    Add new files

---
+++ ObjectOrientedProgramming/Final Words/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string final_words = "Pray for my soul when I depart from this world for we shall come again  ";
+    std::cout << final_words << "\n";
+    return 0;
+}
---
+++ ObjectOrientedProgramming/Last Words/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string final_words = "Pray for my soul when I depart from this world for we shall come again  ";
+
+    return 0;
+}
+
