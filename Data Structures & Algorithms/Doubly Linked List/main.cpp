dfff3cd631bedcf39dfe824466e087b731574b36
GabrielNyawade <gabrielopiyo1133@gmail.com>
2026-03-16 12:21:49 +0100

    Add sorting functions to doubly linked list

--- Data Structures & Algorithms/Doubly Linked List/main.cpp
+++ Data Structures & Algorithms/Doubly Linked List/main.cpp
@@ -29,6 +29,25 @@ void insert_start(int data)
     }
 }
 
+void insert_end(int data)
+{
+    Node* new_node = new Node;
+    new_node->value = data;
+    new_node->prev = nullptr;
+    new_node->next = nullptr;
+    if (start == nullptr) start = new_node;
+    else
+    {
+        Node* ptr = start;
+        while (ptr->next != nullptr)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = new_node;
+        new_node->prev = ptr;
+    }
+}
+
 void display_list()
 {
     std::cout << "NULL";
@@ -38,13 +57,60 @@ void display_list()
     }
     std::cout << "NULL\n";
 }
+void swap(Node* n1, Node* n2)
+{
+    int temp = n1->value;
+    n1->value = n2->value;
+    n2->value = temp;
+}
+
+void ascending(Node* start)
+{
+    Node* p1 = start;
+    while (p1 != nullptr)
+    {
+        Node* p2 = p1->next;
+        while (p2 != nullptr)
+        {
+            if (p2->value < p1->value)
+                swap(p1, p2);
+            p2 = p2->next;
+        }
+        p1 = p1->next;
+    }
+}
+
+void descending(Node* start)
+{
+    Node* p1 = start;
+    while (p1 != nullptr)
+    {
+        Node* p2 = p1->next;
+        while (p2 != nullptr)
+        {
+            if (p2->value > p1->value)
+                swap(p1, p2);
+            p2 = p2->next;
+        }
+        p1 = p1->next;
+    }
+}
 
 int main()
 {
+    insert_start(3);
+    insert_start(1);
     insert_start(2);
-    insert_start(4);
-    insert_start(6);
     insert_start(8);
+    insert_start(9);
+    insert_start(4);
+    insert_start(7);
+    insert_start(5);
+    insert_end(6);
+    display_list();
+    ascending(start);
+    display_list();
+    descending(start);
     display_list();
     return 0;
 }
